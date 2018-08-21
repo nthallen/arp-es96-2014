633de804100c9c174f41855ab9b98f34e5b44151
Norton Allen <allen@huarp.harvard.edu>
2018-08-21 09:05:20 -0600

    Last minute algo changes

--- TM/SerIn/SWcmds.cmd
+++ TM/SerIn/SWcmds.cmd
@@ -2,5 +2,6 @@
   : Scrub Start { $0 = SWS_SCRUB_START; }
   : Set %d { $0 = $2; }
   : Scrub Stop { $0 = SWS_SCRUB_STOP; }
+  : Record { $0 = SWS_RECORD; }
   : Shutdown { $0 = SWS_SHUTDOWN; }
   ;
